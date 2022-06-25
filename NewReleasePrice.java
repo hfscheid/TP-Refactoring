cee61f5193d219d708e597a890884dab748dedd8
hfscheid <henrique.furst@4matt.com.br>
2022-06-25 00:59:16 -0300

    Commit 13

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -1,4 +1,7 @@
 public class NewReleasePrice extends Price {
+   public double getCharge(int daysRented){
+      return daysRented * 3;
+   }
    public int getPriceCode() {
        return Movie.NEW_RELEASE;
    }
